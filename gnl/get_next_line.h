f73ec5c2ee4ed2775519d430c0b6f8db0b3b07df
Lumeris4 <l20langlois@gmail.com>
2024-10-22 16:11:23 +0200

    Update get_next_line.h

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -16,6 +16,10 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+#ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+#endif
+
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
